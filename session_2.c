1bef00aa15e4b7ed24bf2d61d517825a7c8f0240
Jaimin Panchal <jlpanchal299@gmail.com>
2026-08-05 22:16:46 +0530

    Add files via upload

---
+++ session_2.c
@@ -0,0 +1,64 @@
+/* TASKS 
+
+ 1. Install GCC on your computer and verify the installation by running gcc --version in your terminal or command prompt?
+ ANS =  gcc --version
+        PS C:\Users\Jaimin\OneDrive\Desktop\introduction to programming> gcc --version
+        gcc.exe (GCC) 16.1.0
+        Copyright (C) 2026 Free Software Foundation, Inc.
+        This is free software; see the source for copying conditions.  There is NO
+        warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ 2. Open VS Code or DevC++ and create a new file named hello.c. Write a C program that prints 'Hello, Instagram World!' to the console?
+    ANS = #include <stdio.h>
+            int main() {
+                printf("Hello, Instagram World!\n");
+                return 0;
+            }
+
+ 3. Identify and label the header, main function, and return statement in your hello.c program using comments?
+ ANS = #include <stdio.h> // Header file inclusion
+        int main() { // Main function
+            printf("Hello, Instagram World!\n"); // Print statement
+            return 0; // Return statement
+        }
+
+4. Compile your hello.c file using the GCC command line (gcc hello.c -o hello) and run the output file to display the message in your terminal?
+ANS = gcc hello.c
+      PS C:\Users\Jaimin\OneDrive\Desktop\introduction to programming> gcc hello.c  
+      .\a.exe
+      PS C:\Users\Jaimin\OneDrive\Desktop\introduction to programming> .\a.exe      
+      Hello, Instagram world!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */ 
