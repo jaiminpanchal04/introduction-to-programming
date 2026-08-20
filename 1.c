e21163f8db690695caccce3c23f368feab96d9be
Jaimin Panchal <jlpanchal299@gmail.com>
2026-08-20 18:57:17 +0530

    Delete 1.c

--- 1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    char productName[] = "Boat Earphones";
-    float price = 499.99;
-    double rating = 4.5;
-    printf("Product Name (string): %s\n", productName);
-    printf("Price (float): %.2f\n", price);
-    printf("Rating (double): %lf\n", rating);
-}
